16c79c84aa41271a11febfc14dc4f7bb494e3b72
alex.bruns <alex.h.bruns@gmail.com>
2018-05-28 14:42:55 -0500

    added denial function

--- Firewire.js
+++ Firewire.js
@@ -1,29 +1,48 @@
 //////////// GLOBALS ////////////
-users = {};
 transfers = {};
 owner = msg.sender; // init owner as contract creator
 
 //////////// OWNER ONLY FUNCTIONS ////////////
 function issue_eth(topl_address, eth_address, amount) {
-    // check that msg.sender is owner
+    if (msg.sender !== owner) { // owner check
+        throw "NOT OWNER";
+    }
     // create ether assets
     // send ether assets
 }
 
 function approve_transfer(topl_address, eth_address, amount) {
-    if (msg.sender != owner) { // owner check
-        throw "NOT OWNER"
+    if (msg.sender !== owner) { // owner check
+        throw "NOT OWNER";
     }
-    if (transfers.topl_address.eth_adrs === eth_address && transfers.topl_address.balance === amount) {
+    if (transfers.topl_address.eth_adrs === eth_address && transfers.topl_address.balance === amount) { // check that this transfer is what the owner thinks it is
         transfers.topl_address.balance = 0; // reset transfer obj
         transfers.topl_address.active = false;
         // broadcast accepted transfer
+    } else {
+        throw "NOT A VALID TRANSFER";
     }
     // this is called if and only if
     // owner checks that eth_address is a user on the ethereum sidechaining contract
     // then owner checks that that user's listed topl address is the same as the transfer requester's
     // then owner checks that amount is <= to that user's ether balance
+}
 
+function deny_transfer(topl_address, eth_address, amount) {
+    if (msg.sender !== owner) { // owner check
+        throw "NOT OWNER";
+    }
+    if (transfers.topl_address.eth_adrs === eth_address && transfers.topl_address.balance === amount) { // check that this transfer is what the owner thinks it is
+        transfers.topl_address.balance = 0; // reset transfer obj
+        transfers.topl_address.active = false;
+        // broadcast denied transfer
+    } else {
+        throw "NOT A VALID TRANSFER";
+    }
+    // this is called if and only if
+    // owner checks that eth_address is a user on the ethereum sidechaining contract and it is not
+    // or the owner then checks that that user's listed topl address is the same as the transfer requester's and it is not
+    // or the owner then checks that amount is <= to that user's ether balance and it is not
 }
 
 
