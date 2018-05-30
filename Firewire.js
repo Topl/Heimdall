47fa3be0384368f7b59aee846742a5b6387d165f
alex.bruns <alex.h.bruns@gmail.com>
2018-05-30 13:37:21 -0500

    converting to class

--- Firewire.js
+++ Firewire.js
@@ -1,111 +1,111 @@
-//////////// GLOBALS ////////////
-transfers = {};
-owner = contract.owner; // init owner as contract creator (not how this works either)
+//////////// CONTRACT CLASS WRAP ////////////
+var contract = class {
+    //////////// GLOBALS ////////////
+    this.transfers = {};
+    this.owner = contract.owner; // init owner as contract creator (not how this works either)
 
-//////////// OWNER ONLY FUNCTIONS ////////////
-function issue_eth(topl_address, eth_address, amount) {
-    if (msg.sender !== owner) { // owner check
-        throw "NOT OWNER";
+    //////////// OWNER ONLY FUNCTIONS ////////////
+    issue_eth(topl_address, eth_address, amount) {
+        if (msg.sender !== owner) { // owner check
+            throw "NOT OWNER";
+        }
+        // create ether assets
+        // send ether assets
     }
-    // create ether assets
-    // send ether assets
-}
 
-function approve_transfer(topl_address, eth_address, amount) {
-    if (msg.sender !== owner) { // owner check
-        throw "NOT OWNER";
+    approve_transfer(topl_address, eth_address, amount) {
+        if (msg.sender !== owner) { // owner check
+            throw "NOT OWNER";
+        }
+        if (this.transfers.topl_address.eth_adrs === eth_address && this.transfers.topl_address.balance === amount) { // check that this transfer is what the owner thinks it is
+            this.transfers.topl_address.balance = 0; // reset transfer obj
+            this.transfers.topl_address.active = false;
+            // broadcast accepted transfer
+        } else {
+            throw "NOT A VALID TRANSFER";
+        }
+        // this is called if and only if
+        // owner checks that eth_address is a user on the ethereum sidechaining contract
+        // then owner checks that that user's listed topl address is the same as the transfer requester's
+        // then owner checks that amount is <= to that user's ether balance
     }
-    if (transfers.topl_address.eth_adrs === eth_address && transfers.topl_address.balance === amount) { // check that this transfer is what the owner thinks it is
-        transfers.topl_address.balance = 0; // reset transfer obj
-        transfers.topl_address.active = false;
-        // broadcast accepted transfer
-    } else {
-        throw "NOT A VALID TRANSFER";
+
+    deny_transfer(topl_address, eth_address, amount) {
+        if (msg.sender !== owner) { // owner check
+            throw "NOT OWNER";
+        }
+        if (this.transfers.topl_address.eth_adrs === eth_address && this.transfers.topl_address.balance === amount) { // check that this transfer is what the owner thinks it is
+            this.transfers.topl_address.balance = 0; // reset transfer obj
+            this.transfers.topl_address.active = false;
+            // broadcast denied transfer
+        } else {
+            throw "NOT A VALID TRANSFER";
+        }
+        // this is called if and only if
+        // owner checks that eth_address is a user on the ethereum sidechaining contract and it is not
+        // or the owner then checks that that user's listed topl address is the same as the transfer requester's and it is not
+        // or the owner then checks that amount is <= to that user's ether balance and it is not
     }
-    // this is called if and only if
-    // owner checks that eth_address is a user on the ethereum sidechaining contract
-    // then owner checks that that user's listed topl address is the same as the transfer requester's
-    // then owner checks that amount is <= to that user's ether balance
-}
 
-function deny_transfer(topl_address, eth_address, amount) {
-    if (msg.sender !== owner) { // owner check
-        throw "NOT OWNER";
+
+    //////////// PUBLIC FUNCTIONS ////////////
+    start_transfer(eth_address) {
+        var amount = msg.assets.ether; // not how this works.
+        var topl_address = msg.sender; // not how this works either. just go with it.
+        if (this.transfers.topl_address.active === true) {
+            throw "TRANSFER ALREADY IN PROGRESS"
+        } else {
+            this.transfers.topl_address = {"eth_adrs": eth_address, "balance": amount, "active": true};
+        }
+        // broadcast event containing topl_address, eth_address, and amount
     }
-    if (transfers.topl_address.eth_adrs === eth_address && transfers.topl_address.balance === amount) { // check that this transfer is what the owner thinks it is
-        transfers.topl_address.balance = 0; // reset transfer obj
-        transfers.topl_address.active = false;
-        // broadcast denied transfer
-    } else {
-        throw "NOT A VALID TRANSFER";
+
+    get_transfer_status(topl_address) {
+        if (transfers.topl_address.active === true) {
+            return true;
+        }
+        return false;
     }
-    // this is called if and only if
-    // owner checks that eth_address is a user on the ethereum sidechaining contract and it is not
-    // or the owner then checks that that user's listed topl address is the same as the transfer requester's and it is not
-    // or the owner then checks that amount is <= to that user's ether balance and it is not
-}
 
 
-//////////// PUBLIC FUNCTIONS ////////////
-function start_transfer(eth_address) {
-    amount = msg.assets.ether; // not how this works.
-    topl_address = msg.sender; // not how this works either. just go with it.
-    if (transfers.topl_address.active === true) {
-        throw "TRANSFER ALREADY IN PROGRESS"
-    } else {
-        transfers.topl_address = {"eth_adrs": eth_address, "balance": amount, "active": true};
+    //////////// HELPER FUNCTIONS ////////////
+    static safeAdd(a, b) {
+        c = a + b;
+        if (c < a || c < b) {
+            throw "OVER/UNDERFLOW ERROR";
+        } else {
+            return c;
+        }
     }
-    // broadcast event containing topl_address, eth_address, and amount
-}
 
-function get_transfer_status(topl_address) {
-    if (transfers.topl_address.active === true) {
-        return true;
+    static safeSub(a, b) {
+        c = a - b;
+        if (c > a) {
+            throw "OVER/UNDERFLOW ERROR";
+        } else {
+            return c;
+        }
     }
-    return false;
-}
 
-
-//////////// HELPER FUNCTIONS ////////////
-function safeAdd(a, b) {
-    c = a + b;
-    if (c < a || c < b) {
-        throw "OVER/UNDERFLOW ERROR";
-    } else {
-        return c;
+    static safeMul(a, b) {
+        c = a * b;
+        if (a === 0) {
+            return 0;
+        }
+        if (c / a !== b) {
+            throw "OVER/UNDERFLOW ERROR";
+        } else {
+            return c;
+        }
     }
-}
 
-function safeSub(a, b) {
-    c = a - b;
-    if (c > a) {
-        throw "OVER/UNDERFLOW ERROR";
-    } else {
-        return c;
+    static safeDiv(a, b) {
+        c = a / b;
+        if (b === 0) {
+            throw "OVER/UNDERFLOW ERROR";
+        } else {
+            return c;
+        }
     }
-}
-
-function safeMul(a, b) {
-    c = a * b;
-    if (a === 0) {
-        return 0;
-    }
-    if (c / a !== b) {
-        throw "OVER/UNDERFLOW ERROR";
-    } else {
-        return c;
-    }
-}
-
-function safeDiv(a, b) {
-    c = a / b;
-    if (b === 0) {
-        throw "OVER/UNDERFLOW ERROR";
-    } else {
-        return c;
-    }
-}
-
-
-//////////// EVENTS ////////////
+};
 
